a0d4f6521347d75eaa86a3ce454576f08d4a8dec
Fred Dao <freddaoism@gmail.com>
2021-05-01 17:24:48 -0400

    Clock
    JS Clock App

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 setInterval(setClock, 1000)
 
+//Hour hand, Minute hand, and Second hand//
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
 
+//JavaScript Movement//
 function setClock() {
   const currentDate = new Date()
   const secondsRatio = currentDate.getSeconds() / 60
@@ -14,6 +16,7 @@ function setClock() {
   setRotation(hourHand, hoursRatio)
 }
 
+//Rotation Ratio//
 function setRotation(element, rotationRatio) {
   element.style.setProperty('--rotation', rotationRatio * 360)
 }
